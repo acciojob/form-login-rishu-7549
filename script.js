58ccf27d0d358c62b769698541b7883bb16a77ed
Rishu Kumar <126085066+rishu-7549@users.noreply.github.com>
2025-02-06 16:20:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
 
+		let fname = document.querySelector("#fname").value.trim();
+		let lname = document.querySelector("#lname").value.trim();
+
+		alert(`${fname} ${lname}`)
+
 }
